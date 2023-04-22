b22cbe8598d2bbd600636815338a02afdb97f32f
Samuel Reeder <samuel.reeder8@gmail.com>
2023-04-22 19:14:40 -0400

    Login and saved chrome profile

--- src/login.py
+++ src/login.py
@@ -3,26 +3,14 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-import time
-import getpass
-import re
-import sys
-import os
+from selenium.common.exceptions import TimeoutException
+import time, json, re, sys, os
 
+folder_path = os.path.join(os.getcwd(), 'profile')
+os.makedirs(folder_path, exist_ok=True)
 
-def is_valid_email(email):
-    email_pattern = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
-
-    if re.match(email_pattern, email):
-        return True
-    else:
-        return False
-
-def create_visible_driver(session_id, executor_url):
-    driver = webdriver.Chrome()
-    driver.session_id = session_id
-    driver.command_executor._url = executor_url
-    return driver
+chrome_profile_path = folder_path
+snap = "https://web.snapchat.com"
 
 def retry(msg: str) -> bool:
         retry = input(f"{msg} Would you like to retry? y/n")
@@ -36,64 +24,41 @@ def retry(msg: str) -> bool:
             elif sys.platform in ("linux", "linux2", "darwin"):
                 os.system("kill -9 $(ps -p $PPID -o ppid=)")
             return False\
-                
-drive = None
-    
-driver = webdriver.Chrome()
-# chrome_options.add_argument("--headless")
 
-# Create a new instance of the browser driver with the headless option
-# driver = webdriver.Chrome(options=chrome_options)
+def save_session_details(driver, file_name: str) -> None:
+
+    folder_path = os.path.join(os.getcwd(), 'data')
+    print(folder_path)
+    os.makedirs(folder_path, exist_ok=True)
+
+    session_details = {
+        "session_id": driver.session_id,
+        "executor_url": driver.command_executor._url,
+        "url": driver.current_url,
+        "cookies": driver.get_cookies()
+    }
+    with open(os.path.join(folder_path, file_name), "w") as f:
+        json.dump(session_details, f)
+    
+options = webdriver.ChromeOptions()
+options.add_argument(f"user-data-dir={chrome_profile_path}")
+
+driver = webdriver.Chrome(options=options)
 
 while True:
                 
-    # id = input("Please enter your snapchat associated email: ").lower()
-    id = 'samuel.reeder8@gmail.com'
-    password = 'KqK+sua6?C4GQwQ'
-
-    if is_valid_email(id) and password is not None:
-        print("Success")
-        
-    driver.get("https://web.snapchat.com")
+    driver.get(snap)
+    time.sleep(5)
     
-    driver.implicitly_wait(10)
+    try:
+        WebDriverWait(driver, 500).until(lambda d: d.current_url == snap)
+    except TimeoutException:
+        if retry("Timeout exceeded."):
+            continue
 
-    driver.find_element(by=By.ID, value="accountIdentifier").send_keys(id)
-    driver.find_element(by=By.CSS_SELECTOR, value=".primary_action.login-button > button.btn.btn-lg.btn-default").click()
- 
-    # driver.find_element(by=By.ID, value="password").send_keys(password)
-    # driver.find_element(by=By.CSS_SELECTOR, value=".primary_action.login-button > button.btn.btn-lg.btn-default").click()    
-    
-    # print("Waiting...", end="", flush=True)
-    # driver.implicitly_wait(0.5)
-    # print("\r" + " " * len("Waiting..."), end="", flush=True)
-
-    # if "Is This You?" in driver.page_source:
-    #     print("Login successful, 2FA page displayed. Please check your Snapchat app to verify the login. We will then continue.")
-    # else:
-    #     if retry('Login failed.'):
-    #         continue
-
-    # auth_url = driver.current_url
-    success_url = 'https://web.snapchat.com/'
-    WebDriverWait(driver, 500).until(lambda d: d.current_url == success_url)
-    print("Successful")
+    save_session_details(driver, 'session.json')
+    print("Login succesful, session details saved.")
+    time.sleep(10)
+    driver.quit()
     break
-    # try:
-    #     print("Login succesful.")
-    #     # session_id, exec_url= driver.session_id, driver.command_executor._url
-    #     # driver.quit()
-    #     # driver = create_visible_driver(session_id,exec_url)
-    #     break
-    #     # else:
-    #     #     if retry('The 2FA might have been rejected.'):
-    #     #         continue
-    # except WebDriverWait.TimeoutException:
-    #     if retry('Timeout exceeded.'):
-    #         continue
-        
-    # print()
-    
 
-driver.implicitly_wait(1000)
-driver.quit()
--- src/main.py
+++ src/main.py
@@ -1,8 +1,42 @@
 import subprocess
+import json
+from selenium import webdriver
+import time
+import os
 
-condition = True
+def load_session_details(filename):
+    with open(filename, "r") as file:
+        return json.load(file)
 
-if condition:
-    subprocess.run(["python", "src/login.py"])
-else:
-    subprocess.run(["python", "file2.py"])
+def create_driver_with_session(session_details):
+    chrome_profile_path = os.getcwd + '\profile'
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument(f"user-data-dir={chrome_profile_path}")
+    driver = webdriver.Chrome(options=chrome_options)
+    # driver = webdriver.Remote(command_executor=session_details["executor_url"])
+    # driver.session_id = session_details["session_id"]
+    for cookie in session_details["cookies"]:
+        driver.add_cookie(cookie) 
+    return driver
+
+def main() -> None:
+    try:
+        session_details = load_session_details("data/session.json")
+        driver = create_driver_with_session(session_details)
+        driver.get(session_details["url"])
+        # Specify the directory for the Chrome profile
+        
+        
+        # print('hey')
+        time.sleep(1000)
+        # print('he')
+        # driver.quit()
+        driver.quit()
+
+    except Exception as e:
+        print("Please login:", e)
+        subprocess.run(["python", "src/login.py"])
+
+if __name__ == "__main__":
+    main()
